0c3d787ed386867a9d3f85df9cdd4867f26bbcd9
Bhargav Parashar <bhargavparashar99@gmail.com>
2025-07-26 14:24:10 +0530

    update: refactored header code

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { useState, useContext } from "react";
+import {  useContext } from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Stack from "@mui/material/Stack";
